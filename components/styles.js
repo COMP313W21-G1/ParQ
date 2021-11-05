68f392c7540e161cc25ca603ae8b53a4452477b4
shaniquo <61515994+shaniquo@users.noreply.github.com>
2021-11-04 22:48:29 -0400

    updated sign up
    added input fields for sign up

---
+++ components/styles.js
@@ -0,0 +1,15 @@
+
+
+// colors
+export const Colors = {
+  primary: '#ffffff',
+  secondary: '#E5E7EB',
+  tertiary: '#1F2937',
+  darkLight: '#9CA3AF',
+  brand: '#6D28D9',
+  green: '#10B981',
+  red: '#EF4444',
+};
+
+const { primary, secondary, tertiary, darkLight, brand, green, red } = Colors;
+
