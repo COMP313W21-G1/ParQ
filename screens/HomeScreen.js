1a741f2f1ede82e7af3d6fb4a599bd75cbed4d1d
mbutt19 <mbutt19@my.centennialcollege.ca>
2021-11-07 18:13:04 -0500

    Logo integration
    1) Added logo.png
    2) replaced instagram logo with ParQ logo on homescreen,  loginscreen, signupscreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,46 +1,55 @@
-import React from 'react'
-import { StyleSheet, Text, View, SafeAreaView, Image, TouchableOpacity } from 'react-native'
-import SafeViewAndroid from '../components/SafeViewAndroid';
-import tw from 'tailwind-react-native-classnames';
-import NavOptions from '../components/NavOptions';
-import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
-import { GOOGLE_MAPS_APIKEY } from '@env';
-import { useDispatch } from 'react-redux';
-import { setDestination, setOrigin } from '../slices/navSlice';
-import NavFavourites from '../components/NavFavourites';
-import { firebase } from '../firebase';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import SafeViewAndroid from "../components/SafeViewAndroid";
+import tw from "tailwind-react-native-classnames";
+import NavOptions from "../components/NavOptions";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useDispatch } from "react-redux";
+import { setDestination, setOrigin } from "../slices/navSlice";
+import NavFavourites from "../components/NavFavourites";
+import { firebase } from "../firebase";
 // keyboard avoiding view
-import KeyboardAvoidingWrapper from './../components/KeyboardAvoidingWrapper';
+import KeyboardAvoidingWrapper from "./../components/KeyboardAvoidingWrapper";
 
 const HomeScreen = () => {
   const dispatch = useDispatch();
 
   const handleSignout = async () => {
     try {
-      await firebase.auth().signOut().then(() => console.log("Signed Out"));
-      console.log("Signed out successfully!")
+      await firebase
+        .auth()
+        .signOut()
+        .then(() => console.log("Signed Out"));
+      console.log("Signed out successfully!");
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   return (
-
-    <SafeAreaView style={[SafeViewAndroid.AndroidSafeAreaTop, tw`bg-white h-full`]}>
+    <SafeAreaView
+      style={[SafeViewAndroid.AndroidSafeAreaTop, tw`bg-white h-full`]}
+    >
       <View style={tw`p-5`}>
-        <Image 
-          style={{
-            width: 100, height: 100, resizeMode: "contain"
-          }}
-          source={{
-            uri: "https://links.papareact.com/gzs",
-          }}
-        />
-
-        <TouchableOpacity onPress={handleSignout} style={{height: 100, width: 100, backgroundColor: "black"}}>
+        <TouchableOpacity
+          onPress={handleSignout}
+          style={{ height: 150, width: 150, marginBottom: 20 }}
+        >
           <Image
-            style={styles.logo}
-            source={{ uri: "https://img.icons8.com/fluency-systems-regular/60/ffffff/sent.png" }}
+            style={{
+              width: 150,
+              height: 150,
+              resizeMode: "contain",
+            }}
+            source={require("../assets/logo.png")}
           />
         </TouchableOpacity>
 
@@ -77,14 +86,14 @@ const HomeScreen = () => {
         <NavOptions />
         <NavFavourites />
       </View>
-   </SafeAreaView>    
-  )
-}
+    </SafeAreaView>
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
 
 const styles = StyleSheet.create({
   text: {
-    color: "blue"
+    color: "blue",
   },
 });
--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,21 +1,28 @@
-import React from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
-import SignupForm from '../components/signupScreen/SignupForm';
+import React from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
+import SignupForm from "../components/signupScreen/SignupForm";
 // keyboard avoiding view
-import KeyboardAvoidingWrapper from './../components/KeyboardAvoidingWrapper';
-const INSTAGRAM_LOGO = 
+import KeyboardAvoidingWrapper from "./../components/KeyboardAvoidingWrapper";
+const INSTAGRAM_LOGO =
   "https://cdn2.iconfinder.com/data/icons/social-media-2285/512/1_Instagram_colored_svg_1-512.png";
 
-const SignupScreen = ({navigation}) => (
+const SignupScreen = ({ navigation }) => (
   <KeyboardAvoidingWrapper>
-  <View style={styles.container}>
-    <View style={styles.logoContainer}>
-      <Image source={{ uri: INSTAGRAM_LOGO, height: 100, width: 100 }} />
+    <View style={styles.container}>
+      <View style={styles.logoContainer}>
+        <Image
+          style={{
+            width: 150,
+            height: 150,
+            resizeMode: "contain",
+          }}
+          source={require("../assets/logo.png")}
+        />
+      </View>
+      <SignupForm navigation={navigation} />
     </View>
-    <SignupForm navigation={navigation} />
-  </View>
   </KeyboardAvoidingWrapper>
-)
+);
 
 const styles = StyleSheet.create({
   container: {
@@ -30,5 +37,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default SignupScreen
-
+export default SignupScreen;
