0ea0be50d86261b569bf953af71f8819a32b08e8
mbutt19 <mbutt19@my.centennialcollege.ca>
2021-12-14 22:19:04 -0500

    Clean Sync with Ibrahim brach

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { StyleSheet, View, Text, Image } from "react-native";
+import tw from "tailwind-react-native-classnames";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  KeyboardAvoidingView,
+} from "react-native";
 import LoginForm from "../components/loginScreen/LoginForm";
 // keyboard avoiding view
 import KeyboardAvoidingWrapper from "./../components/KeyboardAvoidingWrapper";
@@ -7,21 +14,24 @@ const INSTAGRAM_LOGO =
   "https://cdn2.iconfinder.com/data/icons/social-media-2285/512/1_Instagram_colored_svg_1-512.png";
 
 const LoginScreen = () => (
-  <KeyboardAvoidingWrapper>
-    <View style={styles.container}>
-      <View style={styles.logoContainer}>
-        <Image
-          style={{
-            width: 180,
-            height: 180,
-            resizeMode: "contain",
-          }}
-          source={require("../assets/logo.png")}
-        />
-      </View>
-      <LoginForm />
+  <View style={[styles.container, tw`flex-1 flex-col`]}>
+    <View style={[styles.logoContainer, tw`  h-1/4`]}>
+      <Image
+        style={{
+          width: 180,
+          height: 180,
+          resizeMode: "contain",
+        }}
+        source={require("../assets/logo.png")}
+      />
     </View>
-  </KeyboardAvoidingWrapper>
+
+    <View style={tw``}>
+      <KeyboardAvoidingView style={tw`flex-shrink flex-grow h-3/4`}>
+        <LoginForm />
+      </KeyboardAvoidingView>
+    </View>
+  </View>
 );
 
 const styles = StyleSheet.create({
